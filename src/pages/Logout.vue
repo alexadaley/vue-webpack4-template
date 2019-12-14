d118fdb6f26815f6c1bd3a7fd7ab706f1775d2fe
alexadaley <a96466@aup.edu>
2019-12-14 22:34:37 +0000

    final frontend push

--- src/pages/Logout.vue
+++ src/pages/Logout.vue
@@ -10,3 +10,37 @@ export default {
   }
 }
 </script>
+
+/**
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  mounted () {
+    console.log('Component mounted.')
+  },
+  methods: {
+    formSubmit (e) {
+      e.preventDefault()
+      this.$axios.post('/login', {
+        password: this.password,
+        email: this.email
+      })
+        .then(response => {
+          console.log(response)
+          sessionStorage.userId = JSON.stringify(response.data.data.id)
+          sessionStorage.userName = JSON.stringify(response.data.data.name)
+          console.log(sessionStorage)
+          if (response.data.data.id) {
+            alert('Hello ' + sessionStorage.userName)
+            this.$router.push('/createloggedin')
+          } else { alert('User does not exist. Please enter your username') }
+        })
+    }
+  } }
+</script>
+*/
